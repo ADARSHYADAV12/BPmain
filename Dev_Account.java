bdc183fdbed5ba8aafd33f04a5847d9537a01747
adarsh yadav <37973649+ADARSHYADAV12@users.noreply.github.com>
2021-03-17 10:13:33 +0530

    Update Dev_Account.java

--- Dev_Account.java
+++ Dev_Account.java
@@ -1,5 +1,17 @@
+/**
+ * Class for types of accounts
+ * TESTING FINAL PROJECT
+ * Created by Divyansh Kanishk Adarsh Lakshay Priyanshu Rishi Udit
+ */
+package TestBank;
 public class Dev_Account extends  Dev_MenuOption {
-        
+    /**
+     * 
+     * @param no for account number
+     * @param name for customer name
+     * @param type for account type
+     * @param bal for balance 
+     */    
     public Dev_Account(long no, String name, 
             char type, double bal) {
                 acNo = no;
@@ -7,34 +19,55 @@ public class Dev_Account extends  Dev_MenuOption {
                 acType = type;
                 acBal = bal;
             }
-        
+            /**
+             * 
+             * @return returns the Account number
+             */
             public long getAcNo() {
                 return acNo;
             }
-        
+            /**
+             * 
+             * @return returns account holder name
+             */
             public String getAcHolder() {
                 return acHolderName;
             }
-        
+            /**
+             * 
+             * @return returns account type
+             */
             public char getAcType() {
                 return acType;
             }
-        
+            /**
+             * 
+             * @return returns account balance
+             */
             public double getAcBal() {
                 return acBal;
             }
-        
+            /**
+             * function to create a deposit
+             * @param amt amount to be added
+             */
             public void deposit(double amt) {
                 acBal += amt;
             }
-        
+            /**
+             * 
+             * @param amt amount to be withdrawn
+             * @return returns state of transaction (Either true or false)
+             */
             public boolean withdraw(double amt) {
                 if (acBal < amt)
                     return false;
                 acBal -= amt;
                 return true;
             }
-        
+            /**
+             * To display all the account details
+             */
             public void displayAcDetails() {
                 System.out.println("Account No. : " + acNo);
                 System.out.println("Account Holder Name : " + acHolderName);
